ec98d062de7a8a8e788ffbef27ccb9511d19110b
koroldavid <koroldavid@gmail.com>
2025-03-30 22:20:40 +0300

    control through ros topics

--- Examples_old/ROS/ORB_SLAM3/src/stabilization.cc
+++ Examples_old/ROS/ORB_SLAM3/src/stabilization.cc
@@ -7,21 +7,34 @@
 #include <iostream>
 #include <chrono>
 #include <geometry_msgs/TwistStamped.h>
+#include "std_msgs/Float64.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Empty.h"
 
 ORB_SLAM3::System* slam;
 Sophus::SE3f checkpoint_pose;
 std::atomic<bool> has_checkpoint{false};
 std::atomic<bool> should_save_pose{false};
 std::atomic<bool> shoudl_go_to_pose{false};
+double movement_k = 5.0;
+double rotation_k = 0.25;
 
 ros::Publisher velocity_pub;
 
-void waitForKeyPress() {
-    std::cout << "Press Enter to save your possition after SLAM is ready..." << std::endl;
-    std::cin.get();  // Wait for Enter key
+void movementCallback(const std_msgs::Float64::ConstPtr& msg) {
+    movement_k = msg->data;
+}
+
+void rotationCallback(const std_msgs::Float64::ConstPtr& msg) {
+    rotation_k = msg->data;
+}
+
+void holdCallback(const std_msgs::Bool::ConstPtr& msg) {
+    shoudl_go_to_pose = msg->data;
+}
+
+void saveCurrentPoseCallback(const std_msgs::Empty::ConstPtr& msg) {
     should_save_pose = true;
-    std::cin.get();
-    shoudl_go_to_pose = true;
 }
 
 cv_bridge::CvImageConstPtr ToCvImageMessage(const sensor_msgs::ImageConstPtr& msg) {
@@ -38,7 +51,7 @@ void saveCheckpointPose(const Sophus::SE3f& pose) {
     has_checkpoint = true;
 
     Eigen::Vector3f translation = checkpoint_pose.translation();
-    std::cout << "Initial Pose:\n";
+    std::cout << "Saved Pose:\n";
     std::cout << "x: " << translation.x() << ", y: " << translation.y() << ", z: " << translation.z() << std::endl;
 }
 
@@ -91,13 +104,12 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     Eigen::Vector3f offset = pose.translation() - checkpoint_pose.translation();
     Eigen::Matrix3f rotation_offset = pose.rotationMatrix() * checkpoint_pose.rotationMatrix().transpose();
 
-    // std::cout << "dx: " << offset.x() << ", dy: " << offset.y() << ", dz: " << offset.z() << std::endl;
+    std::cout << "dx: " << offset.x() << ", dy: " << offset.y() << ", dz: " << offset.z() << std::endl;
     std::cout << "norm" << offset.norm() << "\n";
 
     if (offset.norm() < 0.002) return;
 
-    // TODO: read movement coeficient from outside. TO move faster or be more precise e.g. 5
-    geometry_msgs::TwistStamped cmd_vel_stamped = offsetToDroneVelocity(offset, rotation_offset, 5, 0.25);
+    geometry_msgs::TwistStamped cmd_vel_stamped = offsetToDroneVelocity(offset, rotation_offset, movement_k, rotation_k);
     velocity_pub.publish(cmd_vel_stamped);
 }
 
@@ -106,14 +118,23 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "orb_slam3_mono");
     ros::NodeHandle nh;
 
-    std::string vocab_path = "/home/koroldavid/Dev/ORB_SLAM3/Vocabulary/ORBvoc.txt";
-    std::string settings_path = "/home/koroldavid/drones/calibration.yaml";
-    slam = new ORB_SLAM3::System(vocab_path, settings_path, ORB_SLAM3::System::MONOCULAR, true);
+    if(argc != 3)
+    {
+        cerr << endl << "Usage: rosrun ORB_SLAM3 Mono path_to_vocabulary path_to_settings" << endl;        
+        ros::shutdown();
+        return 1;
+    }   
 
-    std::thread input_thread(waitForKeyPress);
+    std::string vocab_path = argv[1];
+    std::string camera_calibration_path = argv[2];
+    slam = new ORB_SLAM3::System(vocab_path, camera_calibration_path, ORB_SLAM3::System::MONOCULAR, true);
 
     velocity_pub = nh.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel", 10);
-    ros::Subscriber sub = nh.subscribe("/webcam/image_raw", 1, imageCallback);
+    ros::Subscriber sub_movement_k = nh.subscribe("/stabilization/movement_k", 1, movementCallback);
+    ros::Subscriber sub_ratoration_k = nh.subscribe("/stabilization/rotation_k", 1, rotationCallback);
+    ros::Subscriber sub_hold = nh.subscribe("/stabilization/enable_hold", 1, holdCallback);
+    ros::Subscriber sub_save_pose = nh.subscribe("/stabilization/save_current_pose", 1, saveCurrentPoseCallback);
+    ros::Subscriber sub_image = nh.subscribe("/webcam/image_raw", 1, imageCallback);
     ros::spin();
     
     slam->Shutdown();
