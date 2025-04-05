63699972d3e6a3fbf8c11bf07945927b5f775cd4
koroldavid <koroldavid@gmail.com>
2025-04-05 16:17:58 +0300

    Try create ubuntu 24 install script

---
+++ make_ubuntu_24_setup.sh
@@ -0,0 +1,58 @@
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt update
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev libjasper-dev
+sudo apt-get install libglew-dev libboost-all-dev libssl-dev
+sudo apt install libeigen3-dev
+sudo apt-get install libcanberra-gtk-module
+
+cd ~
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 4.2.0
+
+sed -i '#define AVFMT_RAWPICTURE 0x0020' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
+sed -i '#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
+sed -i '#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
+
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D WITH_CUDA=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j 3
+sudo make install
+cd ..
+mv opencv opencv4
+
+
+cd ~
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin 
+git checkout 86eb4975fc4fc8b5d92148c2e370045ae9bf9f5d
+mkdir build 
+cd build 
+cmake .. -D CMAKE_BUILD_TYPE=Release 
+make -j 3 
+sudo make install
+
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt install ros-rolling-ros-base
+source /opt/ros/rolling/setup.bash
+echo "source /opt/ros/rolling/setup.bash" >> ~/.bashrc
+
+echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH::~/ORB_SLAM3/Examples_old/ROS/ORB_SLAM3::~/ORB_SLAM3" >> ~/.bashrc
+cd ~/ORB_SLAM3
+./build.sh
+./build_ros.sh
