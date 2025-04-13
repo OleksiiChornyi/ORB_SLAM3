0ec1335cd722b998c3be5e8371334fdbb211f1d2
koroldavid <koroldavid@gmail.com>
2025-04-13 12:12:00 +0300

    Adjust setups for rpi4 with 4 GB

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j 4
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j 4
 
 cd ../../Sophus
 
@@ -21,8 +21,8 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
+make -j 2
 
 cd ../../../
 
@@ -37,4 +37,4 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j 2
--- setup_rpi4_ubuntu_20.sh
+++ setup_rpi4_ubuntu_20.sh
@@ -21,26 +21,28 @@ sudo rosdep init
 rosdep update
 
 # Install Mavros
-sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
+sudo apt-get install ros-noetic-mavros ros-noetic-mavros-extras
 cd ~/
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 chmod a+x install_geographiclib_datasets.sh
-./install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
 
 # Install Camera read node
 cd ~/
 mkdir -p camera_node/src
 cd camera_node/src
 git clone https://github.com/UbiquityRobotics/raspicam_node.git
+cd raspicam_node
+git checkout 0.5.0
 echo "yaml https://raw.githubusercontent.com/UbiquityRobotics/rosdep/master/raspberry-pi.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/30-ubiquity.list > /dev/null
 rosdep update
-cd ..
+cd ~/camera_node
 rosdep install --from-paths src --ignore-src --rosdistro=noetic -y
 catkin_make
 echo "source ~/camera_node/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 # This requires to reboot raspbrerry. Then it will start using a camera.
-echo "start_x=1" | sudo tee -a /boot/config.txt >> /dev/null
+echo "start_x=1" | sudo tee -a /boot/firmware/config.txt >> /dev/null
 
 # Install opencv
 cd ~
@@ -48,17 +50,16 @@ git clone https://github.com/opencv/opencv.git
 cd opencv
 git checkout 4.4.0
 
-sed -i '#define AVFMT_RAWPICTURE 0x0020' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
-sed -i '#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
-sed -i '#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)' | ./modules/videoio/src/cap_ffmpeg_impl.hpp
+hpp_file_to_extend='./modules/videoio/src/cap_ffmpeg_impl.hpp'
+echo "$(echo -en '#define AVFMT_RAWPICTURE 0x0020\n'; cat $hpp_file_to_extend)" > $hpp_file_to_extend
+echo "$(echo -en '#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER\n'; cat $hpp_file_to_extend)" > $hpp_file_to_extend
+echo "$(echo -en '#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)\n'; cat $hpp_file_to_extend)" > $hpp_file_to_extend
 
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=Release -D WITH_CUDA=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j 3
+make -j 4
 sudo make install
-cd ..
-mv opencv opencv4
 
 # Install Pangolin
 cd ~
@@ -68,8 +69,17 @@ git checkout 86eb4975fc4fc8b5d92148c2e370045ae9bf9f5d
 mkdir build 
 cd build 
 cmake .. -D CMAKE_BUILD_TYPE=Release 
-make -j 3 
+make -j 4
 sudo make install
+
+# In case your rpi4 has 4GB memory, allocate extra 4GB swap:
+# sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 
+# sudo chmod 0600 /swapfile 
+# sudo mkswap /swapfile  
+# sudo swapon /swapfile
+
+# Then turn it off after installation:
+# sudo swapoff -a
 
 # Install and build ORB_SLAM3
 sudo apt update && sudo apt install locales
@@ -82,6 +92,7 @@ sudo apt install ros-noetic-tf ros-noetic-image-transport ros-noetic-cv-bridge
 sudo rosdep update
 
 echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH::~/ORB_SLAM3/Examples_old/ROS/ORB_SLAM3" >> ~/.bashrc
+source ~/.bashrc
 cd ~/ORB_SLAM3
 ./build.sh
 ./build_ros.sh
