f42a947c56f092bafcf4ca9e8ec9350d3a387303
OleksiiChornyi <black20011106alex@gmail.com>
2025-08-29 10:54:52 +0000

    stabilization.cc to stereo

--- visual_stabilization/ros2/src/stabilization.cc
+++ visual_stabilization/ros2/src/stabilization.cc
@@ -1,8 +1,12 @@
 #include <rclcpp/rclcpp.hpp>
 
+#include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/compressed_image.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <std_msgs/msg/float64.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <std_msgs/msg/empty.hpp>
@@ -13,46 +17,26 @@
 #include <chrono>
 #include <atomic>
 
+using sensor_msgs::msg::CompressedImage;
 using std::placeholders::_1;
 
 class SLAMStabilizer : public rclcpp::Node {
 public:
     SLAMStabilizer(
-        const std::string& camera_topic_name,
-        const std::string& image_format,
+        const std::string& camera_topic_name_left,
+        const std::string& camera_topic_name_right,
         const std::string& vocab_path,
         const std::string& camera_calibration_path
     ): Node("orb_slam3_stabilization") {
-        slam = new ORB_SLAM3::System(vocab_path, camera_calibration_path, ORB_SLAM3::System::MONOCULAR, true);
 
-        velocity_pub = this->create_publisher<geometry_msgs::msg::TwistStamped>("/mavros/setpoint_velocity/cmd_vel", 10);
+        slam = new ORB_SLAM3::System(vocab_path, camera_calibration_path, ORB_SLAM3::System::STEREO, true);
 
-        sub_movement_k = this->create_subscription<std_msgs::msg::Float64>(
-            "/stabilization/movement_k", 1, std::bind(&SLAMStabilizer::movementCallback, this, _1)
-        );
+        vision_pose_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("/mavros/vision_pose/pose", 10);
 
-        sub_rotation_k = this->create_subscription<std_msgs::msg::Float64>(
-            "/stabilization/rotation_k", 1, std::bind(&SLAMStabilizer::rotationCallback, this, _1)
-        );
-
-        sub_hold = this->create_subscription<std_msgs::msg::Bool>(
-            "/stabilization/enable_hold", 1, std::bind(&SLAMStabilizer::holdCallback, this, _1)
-        );
-
-        sub_save_pose = this->create_subscription<std_msgs::msg::Empty>(
-            "/stabilization/save_current_pose", 1, std::bind(&SLAMStabilizer::saveCurrentPoseCallback, this, _1)
-        );
-
-        if (image_format == "raw") {
-            sub_image = this->create_subscription<sensor_msgs::msg::Image>(
-                camera_topic_name, 1, std::bind(&SLAMStabilizer::rawImageCallback, this, _1)
-            );
-        } else if (image_format == "compressed_jpeg") {
-            sub_compressed_image = this->create_subscription<sensor_msgs::msg::CompressedImage>(
-                camera_topic_name, 1, std::bind(&SLAMStabilizer::compressedImageCallback, this, _1)
-            );
-        }
-
+        img_left_sub.subscribe(this, camera_topic_name_left);
+        img_right_sub.subscribe(this, camera_topic_name_right);
+        image_sync_sub = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(SyncPolicy(10), img_left_sub, img_right_sub);
+        image_sync_sub->registerCallback(&SLAMStabilizer::compressedImageCallback, this);
     }
 
     ~SLAMStabilizer() {
@@ -62,155 +46,117 @@ public:
 
 private:
     ORB_SLAM3::System* slam;
-    Sophus::SE3f checkpoint_pose;
-    std::atomic<bool> has_checkpoint{false};
-    std::atomic<bool> should_save_pose{false};
-    std::atomic<bool> should_go_to_pose{false};
 
-    double movement_k = 5.0;
-    double rotation_k = 0.25;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr vision_pose_pub;
+    
+    message_filters::Subscriber<sensor_msgs::msg::CompressedImage> img_left_sub;
+    message_filters::Subscriber<sensor_msgs::msg::CompressedImage> img_right_sub;
 
-    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr velocity_pub;
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr sub_movement_k;
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr sub_rotation_k;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr sub_hold;
-    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr sub_save_pose;
+    typedef message_filters::sync_policies::ApproximateTime<CompressedImage, CompressedImage> SyncPolicy;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> image_sync_sub;
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_image;
-    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr sub_compressed_image;
+    std::thread stabilization_thread;
 
-    void movementCallback(const std_msgs::msg::Float64::SharedPtr msg) {
-        std::cout << msg->data << "movementCallback:\n";
-        movement_k = msg->data;
-    }
-
-    void rotationCallback(const std_msgs::msg::Float64::SharedPtr msg) {
-        rotation_k = msg->data;
-    }
-
-    void holdCallback(const std_msgs::msg::Bool::SharedPtr msg) {
-        should_go_to_pose = msg->data;
-    }
-
-    void saveCurrentPoseCallback(const std_msgs::msg::Empty::SharedPtr) {
-        should_save_pose = true;
-    }
-
-    void saveCheckpointPose(const Sophus::SE3f& pose) {
-        checkpoint_pose = pose;
-        has_checkpoint = true;
-        should_save_pose = false;
-
-        Eigen::Vector3f translation = checkpoint_pose.translation();
-        std::cout << "Saved Pose:\n";
-        std::cout << "x: " << translation.x() << ", y: " << translation.y() << ", z: " << translation.z() << std::endl;
-    }
-
-    geometry_msgs::msg::TwistStamped offsetToDroneVelocity(
-        const Eigen::Vector3f& offset,
-        const Eigen::Matrix3f& rot_offset
+    void compressedImageCallback(
+        const sensor_msgs::msg::CompressedImage::SharedPtr left,
+        const sensor_msgs::msg::CompressedImage::SharedPtr right
     ) {
-        geometry_msgs::msg::TwistStamped velocity_command;
-        velocity_command.header.stamp = this->now();
-        velocity_command.header.frame_id = "base_link";
-
-        float pitch = std::asin(-rot_offset(2, 0));
-        float yaw = std::atan2(rot_offset(1, 0), rot_offset(0, 0));
-        float roll = std::atan2(rot_offset(2, 1), rot_offset(2, 2));
-        std::cout << "pitch: " << pitch << ", yaw: " << yaw << ", roll: " << roll << std::endl;
-
-        velocity_command.twist.linear.x = offset.y() * movement_k;
-        velocity_command.twist.linear.y = offset.x() * movement_k;
-        velocity_command.twist.linear.z = -offset.z() * movement_k;
-        velocity_command.twist.angular.x = -roll * rotation_k;
-        velocity_command.twist.angular.y = -pitch * rotation_k;
-        velocity_command.twist.angular.z = -yaw * rotation_k;
-        return velocity_command;
-    }
-
-    void processImage(const cv_bridge::CvImageConstPtr& cv_ptr)
-    {
-        double timestamp = rclcpp::Time(cv_ptr->header.stamp).seconds();
-        Sophus::SE3f pose = slam->TrackMonocular(cv_ptr->image,timestamp);
-
-
-        if (pose.translation().norm() == 0) return;
-
-        if (should_save_pose) {
-            saveCheckpointPose(pose);
-        }
-
-        if (!has_checkpoint) return;
-        if (!should_go_to_pose) return;
-
-        Eigen::Vector3f offset = pose.translation() - checkpoint_pose.translation();
-        Eigen::Matrix3f rotation_offset = pose.rotationMatrix() * checkpoint_pose.rotationMatrix().transpose();
-
-        std::cout << "dx: " << offset.x() << ", dy: " << offset.y() << ", dz: " << offset.z() << std::endl;
-        std::cout << "norm" << offset.norm() << "\n";
-
-        if (offset.norm() < 0.002) return;
-
-        geometry_msgs::msg::TwistStamped cmd_vel_stamped = offsetToDroneVelocity(offset, rotation_offset);
-        velocity_pub->publish(cmd_vel_stamped);
-    }
-
-    void rawImageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
-    {
         try {
-            processImage(cv_bridge::toCvShare(msg));
-        } catch (cv_bridge::Exception& e) {
-            throw std::runtime_error("cv_bridge exception: " + std::string(e.what()));
-        }
-    }
+            cv::Mat decoded_image_left = cv::imdecode(cv::Mat(left->data), cv::IMREAD_COLOR);
 
+            cv_bridge::CvImagePtr cv_ptr_left(new cv_bridge::CvImage);
+            cv_ptr_left->image = decoded_image_left;
+            cv_ptr_left->encoding = sensor_msgs::image_encodings::BGR8;
+            cv_ptr_left->header = left->header;
 
-    void compressedImageCallback(const sensor_msgs::msg::CompressedImage::SharedPtr msg)
-    {
-        try {
-            cv::Mat decoded_image = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
+            cv::Mat decoded_image_right = cv::imdecode(cv::Mat(right->data), cv::IMREAD_COLOR);
 
-            cv_bridge::CvImagePtr cv_ptr(new cv_bridge::CvImage);
-            cv_ptr->image = decoded_image;
-            cv_ptr->encoding = sensor_msgs::image_encodings::BGR8;
-            cv_ptr->header = msg->header;
+            cv_bridge::CvImagePtr cv_ptr_right(new cv_bridge::CvImage);
+            cv_ptr_right->image = decoded_image_right;
+            cv_ptr_right->encoding = sensor_msgs::image_encodings::BGR8;
+            cv_ptr_right->header = right->header;
 
-            processImage(cv_ptr);
+            processDataAndStibilize(cv_ptr_left, cv_ptr_right);
         } catch (const std::exception& e) {
             throw std::runtime_error("cv_bridge exception: " + std::string(e.what()));
         }
     }
-};
 
-std::string getValidatedImageFormat(const std::string& image_format)
-{
-    bool is_valid = (image_format == "raw") || (image_format == "compressed_jpeg");
-    if (!is_valid)
-    {
-        cerr << endl << "Error: Invalid image format '" << image_format << "'. Allowed formats: 'raw', 'compressed_jpeg'" << endl;
-        rclcpp::shutdown();
-        exit(1);
+    void processDataAndStibilize(
+        const cv_bridge::CvImageConstPtr& left,
+        const cv_bridge::CvImageConstPtr& right
+    ) {
+        double timestamp = rclcpp::Time(left->header.stamp).seconds();
+        Sophus::SE3f pose = slam->TrackStereo(left->image,right->image,timestamp);
+
+        vision_pose_pub->publish(trasform_slam_pose_to_vision_pose(pose));
     }
 
-    return image_format;
-}
+    geometry_msgs::msg::PoseStamped trasform_slam_pose_to_vision_pose(const Sophus::SE3f& pose) {
+        float roll_cam = 0.0;           // Camera rotation around X (rad)
+        float pitch_cam = 0.0;          // Camera rotation around Y (rad)
+        float yaw_cam = -4.7123889f;    // Camera rotation around Z (rad)
+        float scaleX = 8;
+        float scaleY = 8;
+        float scaleZ = 8;
+        
+        // 1. Scale coordinates to represent real world.
+        Eigen::Vector3f position_orig = pose.translation();
+        Eigen::Matrix3f rotation_orig = pose.rotationMatrix();
+        float gamma_world = std::atan2(rotation_orig(1, 0), rotation_orig(0, 0));
+        
+        // 2. Rotation from original world frame to world frame with Y forward
+        // We use gamma from SLAM yaw, as SLAM switch x and y pose based on its yaw orientation.
+        // Shortly we reset coordinates to direction used on yaw 0, when SLAM was initialized.
+        Eigen::Vector3f position_body;
+        position_body.x() =  std::cos(gamma_world) * position_orig.x() + std::sin(gamma_world) * position_orig.y();
+        position_body.y() = -std::sin(gamma_world) * position_orig.x() + std::cos(gamma_world) * position_orig.y();
+        position_body.z() = position_orig.z();
 
+        // 3. Camera to body frame rotation
+        Eigen::Quaternionf quat_camera = Eigen::Quaternionf(rotation_orig);
+        Eigen::Quaternionf quat_camera_x = Eigen::Quaternionf(Eigen::AngleAxisf(roll_cam,  Eigen::Vector3f::UnitX()));
+        Eigen::Quaternionf quat_camera_y = Eigen::Quaternionf(Eigen::AngleAxisf(pitch_cam, Eigen::Vector3f::UnitY()));
+        Eigen::Quaternionf quat_camera_z = Eigen::Quaternionf(Eigen::AngleAxisf(yaw_cam,   Eigen::Vector3f::UnitZ()));
+
+        Eigen::Quaternionf quat_body = quat_camera * quat_camera_x * quat_camera_y * quat_camera_z;
+        quat_body.normalize();
+
+        // 4. Convert left-handed rotation to right-handed
+        Eigen::Quaternionf quat_right_hand = Eigen::Quaternionf(quat_body.w(),  - quat_body.x(), - quat_body.y(), quat_body.z());
+        quat_right_hand.normalize();
+
+        // Output
+        geometry_msgs::msg::PoseStamped msg_body_pose; 
+        msg_body_pose.header.stamp = this->now();
+        msg_body_pose.header.frame_id = "map";
+        // Convet to WNU to ENU by negating some coordinates
+        msg_body_pose.pose.position.x = -position_body.x() * scaleX;
+        msg_body_pose.pose.position.y = position_body.y() * scaleY;
+        msg_body_pose.pose.position.z = position_body.z() * scaleZ;
+        msg_body_pose.pose.orientation.x = -quat_right_hand.y();
+        msg_body_pose.pose.orientation.y = -quat_right_hand.x();
+        msg_body_pose.pose.orientation.z = quat_right_hand.z();
+        msg_body_pose.pose.orientation.w = quat_right_hand.w();
+        return msg_body_pose;
+    }
+};
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
 
     if(argc != 5) {
-        cerr << endl << "Usage: rosrun ORB_SLAM3 Stabilization $camera_topic_name $image_format $path_to_vocabulary $path_to_settings" << endl;        
+        cerr << endl << "Usage: rosrun ORB_SLAM3 Stabilization $camera_topic_name_left $camera_topic_name_right $path_to_vocabulary $path_to_settings" << endl;        
         rclcpp::shutdown();
         return 1;
     }   
 
-    std::string camera_topic_name = argv[1];
-    std::string image_format = getValidatedImageFormat(argv[2]);
+    std::string camera_topic_name_left = argv[1];
+    std::string camera_topic_name_right = argv[2];
     std::string vocab_path = argv[3];
     std::string camera_calibration_path = argv[4];
 
-    auto node = std::make_shared<SLAMStabilizer>(camera_topic_name, image_format, vocab_path, camera_calibration_path);
+    auto node = std::make_shared<SLAMStabilizer>(camera_topic_name_left, camera_topic_name_right, vocab_path, camera_calibration_path);
 
     rclcpp::spin(node);
     rclcpp::shutdown();
